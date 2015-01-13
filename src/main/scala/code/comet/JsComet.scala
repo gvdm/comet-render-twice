3d96612187c6c9f0e49b5bc4533eb5472532814b
Gustav van der Merwe <gvdm@gvdm.me>
2015-01-13 11:01:52 +0200

    try getting behaviour closer to issue

--- src/main/scala/code/comet/JsComet.scala
+++ src/main/scala/code/comet/JsComet.scala
@@ -3,16 +3,40 @@ package code.comet
 import net.liftweb.http.CometActor
 import net.liftweb.http.S
 import net.liftweb.http.js.JsCmds.Alert
+import net.liftweb.common._
+import net.liftweb.util.Helpers._
+import net.liftweb.http.SHtml._
+import net.liftweb.http.S
+import scala.xml.NodeSeq
+import net.liftweb.builtin.snippet.Comet
+import net.liftweb.http.js.JsCmds._
+import net.liftweb.http.js.jquery.JqJE._
+import net.liftweb.http.js.jquery.JqJsCmds._
+
+case class Msg(text: String)
 
 object JsComet {
-  val template = <div class="text"></div>
+  val template = <div class="comet-button"></div>
 }
 
 class JsComet extends CometActor {
-  
+
   def render = {
     S.appendJs(Alert("should only be here once"))
-    ".text" #> "Comet rendered"
+    ".comet-button" #> ajaxButton("Press here for a closer look", () => {
+      this ! Msg("test")
+      Noop
+    })
+  }
+
+  override def lowPriority = {
+    case Msg(t) ⇒ {
+      S.findOrCreateComet[JsComet](Full(nextFuncName), NodeSeq.Empty, Map.empty, true).map { jsComet ⇒
+        println("made comet "+jsComet.name)
+        val cometHtml = Comet.containerForCometActor(jsComet, Full(JsComet.template))
+        partialUpdate(AppendHtml("comet-holder", cometHtml))
+      }
+    }
   }
 
 }
--- src/main/scala/code/snippet/AddComet.scala
+++ src/main/scala/code/snippet/AddComet.scala
@@ -14,8 +14,7 @@ import net.liftweb.http.js.jquery.JqJsCmds._
 class AddComet {
 
   def render = "#add-comet *" #> ajaxButton("Add comet", () => {
-    
-    val cometHtml = S.findOrCreateComet[JsComet](Full(nextFuncName), NodeSeq.Empty, Map.empty, true).map { jsComet => 
+    val cometHtml = S.findOrCreateComet[JsComet](Full(nextFuncName), NodeSeq.Empty, Map.empty, false).map { jsComet => 
       Comet.containerForCometActor(jsComet, Full(JsComet.template))
     }
     AppendHtml("comet-holder", cometHtml.openOr(<div>ERROR</div>))
